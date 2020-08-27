fbc596f73f5f5239e24db30791d98be7defbae37
Ryan Mattox <509528@gmail.com>
2020-08-27 16:10:37 -0700

    Cell matrix is now a single dimensional array

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 class Cell {
   constructor(x, y) {
-    this.position = [x, y];
     this.state = {
       alive: false,
-      neighbors: [],
       neighborCount: 0,
     }
+    this.position = [x, y];
+    this.neighbors = [];
     this.aliveFrames = 0;
     this.nextState = {};
   }
@@ -37,7 +37,7 @@ class Cell {
   }
   updateNeighborCount() {
     this.state.neighborCount = 0;
-    for(let neighbor of this.state.neighbors) {
+    for(let neighbor of this.neighbors) {
       if(neighbor.state.alive) {
         this.state.neighborCount++;
       }
@@ -47,6 +47,7 @@ class Cell {
 
 class CellMatrix {
   constructor(width, height) {
+    this.matrix = [];
     this.cells = [];
     this.width = width;
     this.height = height;
@@ -56,15 +57,17 @@ class CellMatrix {
     
     // Populate matrix with cells
     for(let y = 0; y < this.height; y++) {
-      this.cells.push([]); 
+      this.matrix.push([]); 
       for(let x = 0; x < this.width; x++) {
-        this.cells[y].push(new Cell(x, y)); 
+        let cell = new Cell(x, y);
+        this.matrix[y].push(cell); 
+        this.cells.push(cell);
       }
     }
     this.addSeed();
     
     // Inform the cells of their neighbors
-    this.forEachCell((cell)=> {
+    this.cells.forEach((cell)=> {
       let neighborCells = [
         [-1, -1], [-1, 0], [-1, 1],
         [0, -1], [0, 1],
@@ -74,25 +77,17 @@ class CellMatrix {
         let x = cell.position[0] + neighbor[0];
         let y = cell.position[1] + neighbor[1];
         if(x > -1 && y > -1 && x < this.width && y < this.height) {
-          cell.state.neighbors.push(this.cells[y][x]);
+          cell.neighbors.push(this.matrix[y][x]);
         }
       }
     });
     
   }
-  forEachCell(func) {
-    for(let y = 0; y < this.height; y++) {
-      for(let x = 0; x < this.width; x++) {
-        func(this.cells[y][x]);
-      }
-    }
-    
-  }
   tick() {
     this.entropy = 0;
     this.aliveCount = 0;
-    this.forEachCell((cell) => cell.tick());
-    this.forEachCell((cell) => {
+    this.cells.forEach((cell) => cell.tick());
+    this.cells.forEach((cell) => {
       cell.useNextState();
       this.entropy += cell.aliveFrames;
       if(cell.state.alive) {
@@ -102,7 +97,7 @@ class CellMatrix {
     this.averageEntropy = this.entropy / this.aliveCount;
   }
   addSeed() {
-    this.forEachCell((cell) => {
+    this.cells.forEach((cell) => {
       cell.state.alive = Math.random() > 0.5;
     });
   }
@@ -111,20 +106,20 @@ class CellMatrix {
 class MatrixViewPort {
   constructor(canvas, cellMatrix, width, height) {
     this.colorCode = [...Array(8).keys()].reverse().map((i) => {
-      return `hsl(${Math.floor(Math.random() * 360)}, ${i/8 * 100}%, ${Math.floor(i/8 * 100)}%)`;
+      return `hsl(${Math.floor((i * 360) / 8)}, ${i/8 * 100}%, ${Math.floor(i/8 * 100)}%)`;
     });
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
     this.width = width;
     this.height = height;
-    this.resize();
     this.cellMatrix = cellMatrix;
+    this.resize();
     window.addEventListener('resize', this.resize.bind(this));
   }
   draw() {
     this.ctx.fillStyle = '#000000';
     this.ctx.fillRect(0, 0,this.canvas.width, this.canvas.height);
-    this.cellMatrix.forEachCell((cell) => {
+    this.cellMatrix.cells.forEach((cell) => {
       if(cell.state.alive == false) {
         return;
       }
@@ -159,7 +154,7 @@ class RadialViewPort extends MatrixViewPort {
   draw() {
     this.ctx.fillStyle = '#000000';
     this.ctx.fillRect(0, 0,this.canvas.width, this.canvas.height);
-    this.cellMatrix.forEachCell((cell) => {
+    this.cellMatrix.cells.forEach((cell) => {
       if(cell.state.alive == false) {
         return;
       }
